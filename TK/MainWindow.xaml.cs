709b4b1bb042e58cdf6b022d23fad33e14de28bf
Dan7Te <danilatelpis3@gmail.com>
2024-04-05 13:26:19 +0300

    Добавьте файлы проекта.

---
+++ TK/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TK {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e) {
+            gridResult.Visibility = Visibility.Visible;
+            Calculate(tbModule1.Text, tbModule2.Text, tbModule3.Text);
+        }
+
+        public bool Calculate(string m1, string m2, string m3) {
+            int im1;
+            int im2;
+            int im3;
+            if(string.IsNullOrEmpty(m1) || string.IsNullOrEmpty(m2) || string.IsNullOrEmpty(m3)) {
+                lblWarning.Content = "Баллы не введены";
+                gridResult.Visibility = Visibility.Hidden;
+                return false;
+            }
+
+            if(!(int.TryParse(m1, out im1) && int.TryParse(m2, out im2) && int.TryParse(m3, out im3))){
+                lblWarning.Content = "Баллы введены неверно";
+                gridResult.Visibility = Visibility.Hidden;
+                return false;
+            }
+
+            if(im1 > 22 || im1 < 0 || im2>38 || im2<0 || im3>20 || im3<0) {
+                lblWarning.Content = "Баллы введены неверно";
+                gridResult.Visibility = Visibility.Hidden;
+                return false;
+            }
+            else {
+                int s = im1 + im2 + im3;
+                if(s >= 0 && s <= 15) lblMark.Content = 2;
+                else if(s>15 && s<=31) lblMark.Content = 3;
+                else if(s > 31 && s <= 55) lblMark.Content = 4;
+                else if(s > 55 && s <= 80) lblMark.Content = 5;
+                return true;
+            }
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TK;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            var window = new MainWindow();
+            Assert.IsFalse(window.Calculate("", "", ""));
+
+            Assert.IsFalse(window.Calculate("12", "", ""));
+            Assert.IsFalse(window.Calculate("", "12", ""));
+            Assert.IsFalse(window.Calculate("", "", "12"));
+
+            Assert.IsFalse(window.Calculate("3", "12", ""));
+            Assert.IsFalse(window.Calculate("", "3", "12"));
+            Assert.IsFalse(window.Calculate("1", "", "5"));
+
+            Assert.IsFalse(window.Calculate("40", "12", "12"));
+            Assert.IsFalse(window.Calculate("12", "12", "40"));
+            Assert.IsFalse(window.Calculate("8", "40", "8"));
+
+            Assert.IsFalse(window.Calculate("-2", "2", "2"));
+            Assert.IsFalse(window.Calculate("3", "-4", "2"));
+            Assert.IsFalse(window.Calculate("4", "1", "-5"));
+
+            Assert.IsFalse(window.Calculate("asdf", "jyt", "rtb"));
+            Assert.IsFalse(window.Calculate("!", "@", "#"));
+            Assert.IsFalse(window.Calculate("2,3", "4.3", "3,3"));
+
+            Assert.IsTrue(window.Calculate("18", "32", "5"));
+            Assert.IsTrue(window.Calculate("12", "12", "12"));
+            Assert.IsTrue(window.Calculate("0", "0", "1"));
+
+        }
+    }
+}
